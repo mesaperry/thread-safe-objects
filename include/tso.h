3e657bf1bde41106f6d78000737f83dd2b4fef46
Mesa <mesperrus@gmail.com>
2021-06-28 03:14:50 -0500

    reformat for cmake

--- include/tso.h
+++ include/tso.h
@@ -5,4 +5,17 @@
 #ifndef THREAD_SAFE_OBJECTS_TSO_H
 #define THREAD_SAFE_OBJECTS_TSO_H
 
+namespace tso {
+
+template <class T>
+class TSO {
+public:
+    TSO(T& object) {
+
+    }
+
+};
+
+}
+
 #endif //THREAD_SAFE_OBJECTS_TSO_H
