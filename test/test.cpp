6b0be05cd6b57c0d13eeee9430295409e2562d52
Mesa Perry <mperry2@snapchat.com>
2021-07-12 21:21:42 -0500

    add unit test

--- test/test.cpp
+++ test/test.cpp
@@ -6,7 +6,26 @@
 
 #include <gtest/gtest.h>
 
-TEST(HelloTest, BasicAssertions) {
-    EXPECT_STRNE("hello", "world");
-    EXPECT_EQ(7 * 6, 42);
+#include <chrono>
+
+TEST(MutexTest, ConcatenateStrings) {
+    tso::Mutex<std::string> obj("");
+    std::string str1 = "fuahwovnfjcwfiijfeowdewgrfvafklnfwevnbviowhcnqoiwdfwe";
+    std::string str2 = "ialhgjonvkjdfnvjcnscqwneojniwvonhnjkbnsvjkcn";
+    auto append = [&obj](std::string suffix){
+        std::scoped_lock lock(obj.getMutex());
+        for (auto ch : suffix) {
+            obj.getObj() += ch;
+            std::this_thread::sleep_for(std::chrono::microseconds(100));
+        }
+    };
+    std::thread t1(append, str1);
+    std::this_thread::sleep_for(std::chrono::microseconds(700));
+    std::thread t2(append, str2);
+    t1.join();
+    t2.join();
+    EXPECT_EQ(obj.getObj().size(), str1.size() + str2.size());
+    for (int i = 0; i < obj.getObj().size(); i++) {
+        EXPECT_EQ(obj.getObj()[i], (str1 + str2)[i]) << "Strings differ at index " << i;
+    }
 }
